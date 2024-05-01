45b92667c0032c33a55602f199da6ec201a5f612
KKKode <kkskumarswain@gmail.com>
2024-05-02 00:48:30 +0530

    on website load component added.

--- src/layouts/public/AdSection.tsx
+++ src/layouts/public/AdSection.tsx
@@ -1,7 +1,10 @@
+import { SignupButton } from "@/components/core";
+
 const AdSection = () => {
   return (
-    <section className="nav-container bg-[#FAF9F6] text-gray-500 text-sm text-center py-4">
-      Shop wholesale online from over 1,00,000 brands. Sign up
+    <section className="nav-container bg-[#FAF9F6] text-gray-500 text-sm text-center py-4 flex items-center justify-center gap-2">
+      <p>Shop wholesale online from over 1,00,000 brands. </p>
+      <SignupButton title="Sign Up" className="underline" />
     </section>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb } from "@/components/core";
+import { Breadcrumb, CustomModal, SigninButton } from "@/components/core";
 import {
   Advertisement,
   ExploreSection,
@@ -6,10 +6,60 @@ import {
   FeatureSection,
 } from "@/components/home";
 import { PublicLayout } from "@/layouts";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOpen(true);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <PublicLayout>
+      <CustomModal open={open} onClose={() => setOpen(false)}>
+        <div className="flex justify-end p-2">
+          <button onClick={() => setOpen(false)}>❌</button>
+        </div>
+        <div className="p-4 md:p-6 lg:pt-4 lg:pb-12 lg:px-12 rounded-md w-[40vw]">
+          <div className="flex flex-col items-center text-center gap-3">
+            <img src="/faire-logo.svg" alt="logo" className="w-28" />
+            <h2 className="title">Shop wholesale, online</h2>
+            <p className="description">
+              Purchase from over 1,00,000 unique brands. Sign up for free!
+            </p>
+          </div>
+          <form className="pt-8 pb-4">
+            <label
+              htmlFor="email"
+              className="text-sm text-gray-600 tracking-wider"
+            >
+              Business email address
+            </label>
+            <div className="border rounded-sm border-gray-600 px-3 py-2 mt-1 mb-3">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="you@yourstore.com"
+                className="w-full outline-none"
+              />
+            </div>
+            <button type="submit" className="btn-primary w-full">
+              Sign up for free
+            </button>
+          </form>
+          <div className="text-center">
+            <SigninButton className="underline" />
+          </div>
+          <div className="flex items-center gap-4 mt-2">
+            <span className="bg-gray-200 w-full h-[1px]"></span>
+            <span>or</span>
+            <span className="bg-gray-200 w-full h-[1px]"></span>
+          </div>
+        </div>
+      </CustomModal>
       <main>
         <Breadcrumb />
         <FeatureSection />
